fd4f1ade7973d38329c293acb4b84ee4470075a9
Bryan Tabares <bryan.tabares@PDS.COM.PH>
2024-04-01 11:45:25 +0800

    added tokyonight and vscode themes | vscode is default

--- lua/ajax/packer.lua
+++ lua/ajax/packer.lua
@@ -8,18 +8,34 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.6',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
+          'nvim-telescope/telescope.nvim', tag = '0.1.6',
+          -- or                            , branch = '0.1.x',
+          requires = { {'nvim-lua/plenary.nvim'} }
   }
 
+--  use ({
+--          'rose-pine/neovim',
+--          as = 'rose-pine',
+--          config = function()
+--                  vim.cmd('colorscheme rose-pine')
+--          end
+--  })
+
+--  use ({
+--      'folke/tokyonight.nvim',
+--      as = 'tokyonight',
+--      config = function()
+--          vim.cmd('colorscheme tokyonight-storm')
+--      end
+--  })
   use ({
-	  'rose-pine/neovim',
-	  as = 'rose-pine',
-	  config = function()
-		  vim.cmd('colorscheme rose-pine')
-	  end
+      'askfiy/visual_studio_code',
+      as = 'visual_studio_code',
+      config = function()
+          vim.cmd('colorscheme visual_studio_code')
+      end
   })
+
   use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   use ('nvim-treesitter/playground')
   use ('theprimeagen/harpoon')
@@ -28,24 +44,25 @@ return require('packer').startup(function(use)
   use ('tpope/vim-fugitive')
 
   use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v3.x',
-	  requires = {
-		  --- Uncomment the two plugins below if you want to manage the language servers from neovim
-		  {'neovim/nvim-lspconfig'},
-		  {'williamboman/mason.nvim'},
-		  {'williamboman/mason-lspconfig.nvim'},
+          'VonHeikemen/lsp-zero.nvim',
+          branch = 'v3.x',
+          requires = {
+                  --- Uncomment the two plugins below if you want to manage the language servers from neovim
+                  {'neovim/nvim-lspconfig'},
+                  {'williamboman/mason.nvim'},
+                  {'williamboman/mason-lspconfig.nvim'},
 
-		  {'hrsh7th/nvim-cmp'},
-		  {'hrsh7th/cmp-nvim-lsp'},
-		  {'hrsh7th/cmp-buffer'},
-		  {'hrsh7th/cmp-path'},
-		  {'saadparwaiz1/cmp_luasnip'},
-		  {'hrsh7th/cmp-nvim-lua'},
-		  {'L3MON4D3/LuaSnip'},
-		  {'rafamadriz/friendly-snippets'},
-	  }
+                  {'hrsh7th/nvim-cmp'},
+                  {'hrsh7th/cmp-nvim-lsp'},
+                  {'hrsh7th/cmp-buffer'},
+                  {'hrsh7th/cmp-path'},
+                  {'saadparwaiz1/cmp_luasnip'},
+                  {'hrsh7th/cmp-nvim-lua'},
+                  {'L3MON4D3/LuaSnip'},
+                  {'rafamadriz/friendly-snippets'},
+          }
   }
 
   use ('theprimeagen/vim-be-good')
+
 end)
