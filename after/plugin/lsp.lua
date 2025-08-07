d39f871a9de2d29fe868fdac1f27dc7591ab16d0
Bryan Tabares <tabaresbryan39@gmail.com>
2025-08-07 15:30:01 +0800

    added kitty config

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -22,8 +22,7 @@ require('mason-lspconfig').setup({
 		lua_ls = function()
 			local lua_opts = lsp.nvim_lua_ls()
 			require('lspconfig').lua_ls.setup(lua_opts)
-		end,
-	}
+		end, }
 })
 
 local cmp = require('cmp')
